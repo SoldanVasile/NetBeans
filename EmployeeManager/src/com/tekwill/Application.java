048ece5c929cf7b51a31faa79641acd18b3230c0
Vasile SOLDAN <soldan.vasile@gmail.com>
2019-06-10 21:16:43 +0300

    Creare repositoriu NetBeans

---
+++ EmployeeManager/src/com/tekwill/Application.java
@@ -0,0 +1,14 @@
+package com.tekwill;
+/**
+ *
+ * @author User
+ */
+public class Application {
+    
+    public static void main (String [] args){
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
